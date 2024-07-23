649edc90518c19231831ea625d82f3b0d3d955d5
Jacelynn <jacelynn.doan123@gmail.com>
2024-07-23 02:38:55 -0600

    displayable and advertisement class implementation

---
+++ src/ca/ucalgary/edu/ensf380/Advertisements.java
@@ -0,0 +1,10 @@
+package ca.ucalgary.edu.ensf380;
+
+public class Advertisements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ca/ucalgary/edu/ensf380/Displayable.java
@@ -0,0 +1,10 @@
+package ca.ucalgary.edu.ensf380;
+
+public class Displayable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
