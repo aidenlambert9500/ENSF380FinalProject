6583330213fa449ff774d683052fb07eb3d3dd2a
Jacelynn <jacelynn.doan123@gmail.com>
2024-07-31 01:36:38 -0600

    updates to line and train class

--- src/ca/ucalgary/edu/ensf380/Line.java
+++ src/ca/ucalgary/edu/ensf380/Line.java
@@ -1,10 +1,67 @@
 package ca.ucalgary.edu.ensf380;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Line {
+    private final String lineName;
+    private final List<Train> trains;
+    private final List<Station> stations;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+    public Line(String lineName, List<Station> stations, List<Train> trains) {
+        this.lineName = lineName;
+        this.stations = Collections.unmodifiableList(stations);
+        this.trains = Collections.unmodifiableList(trains);
+    }
 
+    
+    public Line(String lineName, List<Station> stations) {
+        this.lineName = lineName;
+        this.stations = Collections.unmodifiableList(stations);
+        this.trains = null;
+    }
+
+
+    public Line(String lineName) {
+        this.lineName = lineName;
+        this.trains = null;
+        this.stations = new ArrayList<>();
+    }
+
+
+    public String getLineName() {
+        return lineName;
+    }
+
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+ 
+    public List<Train> getTrains() {
+        return trains;
+    }
+
+
+    public void addStation(Station station) {
+        this.stations.add(station);
+    }
+
+
+    public Station getStationByCode(String stationCode) {
+        Station matchingTrainStation = null;
+        for (Station station : stations) {
+            if (station.getStationCode().equals(stationCode)) {
+                matchingTrainStation = station;
+                break;
+            }
+        }
+        return matchingTrainStation;
+    }
 }
+
+	
--- src/ca/ucalgary/edu/ensf380/Train.java
+++ src/ca/ucalgary/edu/ensf380/Train.java
@@ -1,10 +1,54 @@
 package ca.ucalgary.edu.ensf380;
 
+
 public class Train {
+    private final String trainNum;
+    private final Line currentLine;
+    private String direction;
+    private String destination;
+    private Station currentStation;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+    public Train(String trainNum, Line currentLine, String direction, String destination) {
+        this.trainNum = trainNum;
+        this.currentLine = currentLine;
+        this.direction = direction;
+        this.destination = destination;
+    }
 
+
+    public String getTrainNum() {
+        return trainNum;
+    }
+
+
+    public Line getCurrentLine() {
+        return currentLine;
+    }
+
+
+    public String getDirection() {
+        return direction;
+    }
+    
+
+    public Station getCurrentStation() {
+        return currentStation;
+    }
+
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+
+    public void setCurrentStation(Station currentStation) {
+        this.currentStation = currentStation;
+    }
+
+
+    public String getCurrentStationCode() {
+        String statCode = this.currentStation.getStationCode();
+        return statCode;
+    }
 }
